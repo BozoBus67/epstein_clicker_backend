e8535ca28bdf3627b41a32e782230e557fbc390f
BozoBus67 <666handsomeguy999@gmail.com>
2026-04-27 02:51:41 -0400

    miscellaneous dev work

--- initializations_and_declarations/db_initialization.py
+++ initializations_and_declarations/db_initialization.py
@@ -1,8 +1,10 @@
 # db.py
 import os
 from supabase import create_client
+from supabase.lib.client_options import ClientOptions
 
 supabase = create_client(
   os.getenv("SUPABASE_URL"),
-  os.getenv("SUPABASE_SERVICE_KEY")
+  os.getenv("SUPABASE_SERVICE_KEY"),
+  options=ClientOptions(auto_refresh_token=False, persist_session=False)
 )
