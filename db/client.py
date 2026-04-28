7a16a89f3b6ad1ece2d2c88791f080802b10be45
BozoBus67 <666handsomeguy999@gmail.com>
2026-04-28 19:14:51 -0400

    miscellaneous dev work

--- db/client.py
+++ db/client.py
@@ -1,7 +1,11 @@
 import os
-from supabase import create_client
+from supabase import create_client, ClientOptions
 
 supabase = create_client(
   os.getenv("SUPABASE_URL"),
-  os.getenv("SUPABASE_SECRET_KEY")
+  os.getenv("SUPABASE_SECRET_KEY"),
+  options=ClientOptions(
+    auto_refresh_token=False,
+    persist_session=False,
+  )
 )
