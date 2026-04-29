e384610b0bb3ff85485611eed5ccabe9aafde362
BozoBus67 <666handsomeguy999@gmail.com>
2026-04-28 20:05:11 -0400

    miscellaneous dev work

--- data/account_tiers.py
+++ data/account_tiers.py
@@ -1,11 +1,11 @@
-import constants.constants as Constants
+import constants.constants as C
 
 ACCOUNT_TIERS = [
-  {"id": Constants.ACCOUNT_TIER_0_ID, "label": Constants.ACCOUNT_TIER_0_LABEL, "token_price": Constants.ACCOUNT_TIER_0_TOKEN_PRICE},
-  {"id": Constants.ACCOUNT_TIER_1_ID, "label": Constants.ACCOUNT_TIER_1_LABEL, "token_price": Constants.ACCOUNT_TIER_1_TOKEN_PRICE},
-  {"id": Constants.ACCOUNT_TIER_2_ID, "label": Constants.ACCOUNT_TIER_2_LABEL, "token_price": Constants.ACCOUNT_TIER_2_TOKEN_PRICE},
-  {"id": Constants.ACCOUNT_TIER_3_ID, "label": Constants.ACCOUNT_TIER_3_LABEL, "token_price": Constants.ACCOUNT_TIER_3_TOKEN_PRICE},
-  {"id": Constants.ACCOUNT_TIER_4_ID, "label": Constants.ACCOUNT_TIER_4_LABEL, "token_price": Constants.ACCOUNT_TIER_4_TOKEN_PRICE},
-  {"id": Constants.ACCOUNT_TIER_5_ID, "label": Constants.ACCOUNT_TIER_5_LABEL, "token_price": Constants.ACCOUNT_TIER_5_TOKEN_PRICE},
-  {"id": Constants.ACCOUNT_TIER_6_ID, "label": Constants.ACCOUNT_TIER_6_LABEL, "token_price": Constants.ACCOUNT_TIER_6_TOKEN_PRICE},
+  {"id": C.ACCOUNT_TIER_0_ID, "token_price": C.ACCOUNT_TIER_0_TOKEN_PRICE},
+  {"id": C.ACCOUNT_TIER_1_ID, "token_price": C.ACCOUNT_TIER_1_TOKEN_PRICE},
+  {"id": C.ACCOUNT_TIER_2_ID, "token_price": C.ACCOUNT_TIER_2_TOKEN_PRICE},
+  {"id": C.ACCOUNT_TIER_3_ID, "token_price": C.ACCOUNT_TIER_3_TOKEN_PRICE},
+  {"id": C.ACCOUNT_TIER_4_ID, "token_price": C.ACCOUNT_TIER_4_TOKEN_PRICE},
+  {"id": C.ACCOUNT_TIER_5_ID, "token_price": C.ACCOUNT_TIER_5_TOKEN_PRICE},
+  {"id": C.ACCOUNT_TIER_6_ID, "token_price": C.ACCOUNT_TIER_6_TOKEN_PRICE},
 ]
